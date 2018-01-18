fbf49205def0bc0744c00a9058000960e436baef
vincent <vincent4502237@gmail.com>
2018-01-18 09:36:20 +0800

    first commit

---
+++ quick-start/src/main/kotlin/com/person/vincent/QuickStartApplication.kt
@@ -0,0 +1,20 @@
+package com.person.vincent
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class QuickStartApplication {
+
+    @GetMapping("/say")
+    fun say(): String {
+        return "Hello World!!!"
+    }
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(QuickStartApplication::class.java, *args)
+}
